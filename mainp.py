ec7fe1d64885a9a44e2272128ec5849062bee144
Perrofa <111369005+Perrofa@users.noreply.github.com>
2022-10-07 16:14:18 -0500

    Se uso las listas anidadas en los jugadores

--- mainp.py
+++ mainp.py
@@ -5,6 +5,7 @@ description: funcion para calificar preguntas
 params:num:pregunta, respuesta, puntaje
 return: puntaje
 """
+
 def calificar_respueta(num_pregunta, respuesta, puntaje):
   if(num_pregunta == 1):
     if respuesta == "si":
@@ -42,11 +43,23 @@ def calificar_respueta(num_pregunta, respuesta, puntaje):
     else:
       puntaje = puntaje - 1
   return puntaje
+
+def printResultados():
+  #print("#          Username          Score")
+  for jugador in jugadores:
+    #print("jugador numero " + str(jugador[0]) + " => " + jugador[1] + " => obtuviste un puntaje de: " + str(jugador[2]))
+    #print(str(jugador[0]) + "          " + jugador[1] + "         " + str(jugador[2]))
+    print("Jugador numero: " + str(jugador[0]))
+    print("Username: " + jugador[1])
+    print("Puntaje: " + str(jugador[2]))
+    print("======================================================================================")
+
      
 
 puntaje = 0
 respuesta = ""
-jugadores = 0
+num_jugadores = 0
+jugadores = []
 preguntas= [": El presidente de Mexico es Andres Manuel Lopez Obrador? (si/no)",
             ": Benito Juarez era muy alto? (si/no)",
             ": El arcoiris tiene 7 colores? (si/no)",
@@ -56,39 +69,41 @@ preguntas= [": El presidente de Mexico es Andres Manuel Lopez Obrador? (si/no)",
 
 
 print("Cuantos jugadores son?")
-jugadores = int(input())
+num_jugadores = int(input())
 
-while jugadores != 0:
+while num_jugadores != 0:
   puntaje = 0
-  
-  print("Jugador " + str(jugadores) + preguntas[0])
+
+  print("Jugador " + str(num_jugadores) + ": Cual es tu username?")
+  nombre = input()
+
+  print("Jugador " + str(num_jugadores) + preguntas[0])
   respuesta = input()
   puntaje = calificar_respueta(1,respuesta, puntaje)
   
-  print("Jugador " + str(jugadores) + preguntas[1])
+  print("Jugador " + str(num_jugadores) + preguntas[1])
   respuesta = input()
   puntaje = calificar_respueta(2,respuesta,puntaje)
 
-  print("Jugador " + str(jugadores) + preguntas[2])
+  print("Jugador " + str(num_jugadores) + preguntas[2])
   respuesta = input()
   puntaje = calificar_respueta(3,respuesta,puntaje)
 
-  print("Jugador " + str(jugadores) + preguntas[3])
+  print("Jugador " + str(num_jugadores) + preguntas[3])
   respuesta = input()
   puntaje = calificar_respueta(4,respuesta,puntaje)
 
-  print("Jugador " + str(jugadores) + preguntas[4])
+  print("Jugador " + str(num_jugadores) + preguntas[4])
   respuesta = input()
   puntaje = calificar_respueta(5,respuesta,puntaje)
 
-  print("Jugador " + str(jugadores) + preguntas[5])
+  print("Jugador " + str(num_jugadores) + preguntas[5])
   respuesta = input()
   puntaje = calificar_respueta(6,respuesta,puntaje)
+
+  jugadores.append([num_jugadores,nombre,puntaje])
   
-  print("Jugador " + str(jugadores) + " Puntuaje es igual a " + str(puntaje))
   print("======================================================================================")
-  jugadores = jugadores - 1
-  
+  num_jugadores = num_jugadores - 1
 
-
-
+printResultados()
