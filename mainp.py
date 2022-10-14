935d332273f0370037b49aeb09222b9ad866d247
Perrofa <111369005+Perrofa@users.noreply.github.com>
2022-10-13 19:03:40 -0500

    Atribución y formateo de codígo.

--- mainp.py
+++ mainp.py
@@ -7,7 +7,7 @@ return: puntaje
 """
 
 def calificar_respueta(num_pregunta, respuesta, puntaje):
-  if(num_pregunta == 1):
+  if (num_pregunta == 1):
     if respuesta == "si":
       puntaje = puntaje + 1
     else:
@@ -44,6 +44,10 @@ def calificar_respueta(num_pregunta, respuesta, puntaje):
       puntaje = puntaje - 1
   return puntaje
 
+"""
+description: funcion para imprimir resultados
+"""
+
 def printResultados():
   #print("#          Username          Score")
   for jugador in jugadores:
@@ -55,7 +59,9 @@ def printResultados():
     print("======================================================================================")
 
      
-
+"""
+Variables
+"""
 puntaje = 0
 respuesta = ""
 num_jugadores = 0
@@ -67,43 +73,48 @@ preguntas= [": El presidente de Mexico es Andres Manuel Lopez Obrador? (si/no)",
             ": Una lista es inmutable? (si/no)",
             ": Final Fantasy es el mejor juego del mundo? (si/no)"]
 
+if __name__ == '__main__':
+  print("Cuantos jugadores son?")
+  num_jugadores = int(input())
 
-print("Cuantos jugadores son?")
-num_jugadores = int(input())
+  while num_jugadores != 0:
+    puntaje = 0
 
-while num_jugadores != 0:
-  puntaje = 0
+    print("Jugador " + str(num_jugadores) + ": Cual es tu username?")
+    nombre = input()
 
-  print("Jugador " + str(num_jugadores) + ": Cual es tu username?")
-  nombre = input()
+    print("Jugador " + str(num_jugadores) + preguntas[0])
+    respuesta = input()
+    puntaje = calificar_respueta(1,respuesta, puntaje)
+    
+    print("Jugador " + str(num_jugadores) + preguntas[1])
+    respuesta = input()
+    puntaje = calificar_respueta(2,respuesta,puntaje)
 
-  print("Jugador " + str(num_jugadores) + preguntas[0])
-  respuesta = input()
-  puntaje = calificar_respueta(1,respuesta, puntaje)
-  
-  print("Jugador " + str(num_jugadores) + preguntas[1])
-  respuesta = input()
-  puntaje = calificar_respueta(2,respuesta,puntaje)
+    print("Jugador " + str(num_jugadores) + preguntas[2])
+    respuesta = input()
+    puntaje = calificar_respueta(3,respuesta,puntaje)
 
-  print("Jugador " + str(num_jugadores) + preguntas[2])
-  respuesta = input()
-  puntaje = calificar_respueta(3,respuesta,puntaje)
+    print("Jugador " + str(num_jugadores) + preguntas[3])
+    respuesta = input()
+    puntaje = calificar_respueta(4,respuesta,puntaje)
 
-  print("Jugador " + str(num_jugadores) + preguntas[3])
-  respuesta = input()
-  puntaje = calificar_respueta(4,respuesta,puntaje)
+    print("Jugador " + str(num_jugadores) + preguntas[4])
+    respuesta = input()
+    puntaje = calificar_respueta(5,respuesta,puntaje)
 
-  print("Jugador " + str(num_jugadores) + preguntas[4])
-  respuesta = input()
-  puntaje = calificar_respueta(5,respuesta,puntaje)
+    print("Jugador " + str(num_jugadores) + preguntas[5])
+    respuesta = input()
+    puntaje = calificar_respueta(6,respuesta,puntaje)
 
-  print("Jugador " + str(num_jugadores) + preguntas[5])
-  respuesta = input()
-  puntaje = calificar_respueta(6,respuesta,puntaje)
+    jugadores.append([num_jugadores,nombre,puntaje])
+    
+    print("======================================================================================")
+    num_jugadores = num_jugadores - 1
 
-  jugadores.append([num_jugadores,nombre,puntaje])
-  
-  print("======================================================================================")
-  num_jugadores = num_jugadores - 1
+  printResultados()
 
-printResultados()
+"""
+Referencias: 
+  API Python. (2022, 11 octubre). Python.org. Recuperado 13 de octubre de 2022, de https://www.python.org/
+"""
